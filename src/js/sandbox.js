98b35dca7901b7806e0bd1696ca763b700ef8113
Mat <mathew.teague@yewtreeweb.co.uk>
2020-04-24 13:07:00 +0100

    functions,

--- src/js/sandbox.js
+++ src/js/sandbox.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-new-object */
+/* eslint-disable dot-notation */
+/* eslint-disable no-array-constructor */
 /* eslint-disable no-alert */
 /* eslint-disable no-unused-vars */
 
@@ -83,7 +86,7 @@ x += 1
 x++
 console.log(x)
 
-// Coding Challenge
+// / Coding Challenge 1
 const matHeight = 1.95 // metres
 const matWeight = 98.09 // kilograms
 const aliceHeight = 1.56 // metres
@@ -169,3 +172,246 @@ switch (true) {
  * ==: Does type coercion will convert one value to match another e.g. 23 == '23'
  * ===: strict, does not type coercion
  */
+
+// / Coding Challenge 2
+
+// Get average score.
+const johnScore = Math.round((89 + 120 + 103) / 3)
+const mikeScore = Math.round((116 + 94 + 123) / 3)
+const maryScore = Math.round((97 + 134 + 105) / 3)
+
+const score = document.querySelector('.score')
+
+if (johnScore > mikeScore && johnScore > maryScore) {
+  console.log(`John's team is the winner! With a score of: ${johnScore}`)
+  score.innerHTML += `<p>John's team is the winner! With a score of <span>${johnScore}</span></p>`
+} else if (mikeScore > johnScore && mikeScore > maryScore) {
+  console.log(`Mike's team is the winner! With a score of: ${mikeScore}`)
+  score.innerHTML += `<p>Mike's team is the winner! With a score of <span>${mikeScore}</span></p>`
+} else if (maryScore > johnScore && maryScore > mikeScore) {
+  console.log(`Mary's team is the winner! With a score of: ${maryScore}`)
+  score.innerHTML += `<p>Mary's team is the winner! With a score of <span>${maryScore}</span></p>`
+} else {
+  console.log('There has been a draw!')
+  score.innerHTML += `<p>There has been a draw!</p>`
+}
+
+// Functions
+const calculateAge = (birthYear) => {
+  const year = new Date().getFullYear()
+  return year - birthYear - 1
+}
+const retirement = (year, name) => {
+  const yearOfBirth = calculateAge(year)
+  const retire = 70 - yearOfBirth
+  if (retire > 0) {
+    console.log(`${name} retires in ${retire} years`)
+  } else {
+    console.log(`${name} has retired or is due for retirement`)
+  }
+}
+
+retirement(1989, 'Mat')
+
+// / Function statements and expressions
+
+/**
+ *
+ * Function declartion
+ * funciton name (yourName) {}
+ *
+ * Function expression
+ * const name = function (yourName) {}
+ */
+const whatDoYouDo = (role, name) => {
+  switch (role) {
+    case 'coder':
+      return `${name} codes for fun and work`
+    case 'teacher':
+      return `${name} teaches people new things`
+    case 'vet':
+      return `${name} helps sick animals`
+
+    default:
+      return `${name} does something else`
+  }
+}
+
+console.log(whatDoYouDo('coder', 'Mat'))
+
+// / Arrays
+
+// Initialize array
+const names = ['Mat', 'Alice', 'Lily']
+const years = new Array(1989, 1990, 2018)
+
+console.log(names)
+
+// Mutate Array
+names[names.length] = 'Pam'
+
+names.push('Jeff') // adds element to end of array
+names.unshift('Pepsi') // adds element to the start of array
+console.log(names)
+
+names.pop() // removes the end element
+names.shift() // removes the start element
+console.log(names)
+
+console.log(names.indexOf('Lily')) // returns the position of the element within the array
+
+const hasLily = names.includes('Lily') ? 'Has Lily' : "Dosen't have Lily inside"
+console.log(hasLily)
+
+// / Coding Challenge 3
+
+const tips = []
+const bills = []
+const tipping = (bill) => {
+  let tip
+  if (bill < 50) {
+    tip = Math.round(bill * (20 / 100))
+  } else if (bill >= 50 && bill < 200) {
+    tip = Math.round(bill * (15 / 100))
+  } else {
+    tip = Math.round(bill * (10 / 100))
+  }
+
+  tips.push(`£${tip}`)
+
+  const total = `£${bill + tip}`
+  bills.push(total)
+}
+
+tipping(124)
+tipping(48)
+tipping(268)
+console.log(tips)
+console.log(bills)
+
+// How instructor would do it
+const tipCalculator = (bill) => {
+  let percentage
+  if (bill < 50) {
+    percentage = 0.2
+  } else if (bill >= 50 && bill < 200) {
+    percentage = 0.15
+  } else {
+    percentage = 0.1
+  }
+  return Math.round(percentage * bill) // I've round it to stop the crazy decimal points.
+}
+
+const instructorBills = [124, 48, 268]
+const instructorTips = [
+  tipCalculator(instructorBills[0]),
+  tipCalculator(instructorBills[1]),
+  tipCalculator(instructorBills[2]),
+]
+const finalValues = [
+  instructorBills[0] + instructorTips[0],
+  instructorBills[1] + instructorTips[1],
+  instructorBills[2] + instructorTips[2],
+]
+console.log(instructorTips)
+console.log(finalValues)
+
+// / Objects
+const person = {
+  firstName: 'Mathew',
+  middleName: 'Adam',
+  lastName: 'Teague',
+  birthYear: 1989,
+  family: ['Alice', 'Lily', 'Pam', 'Tressa', 'Chloe', 'Sophie'],
+  job: 'UI Developer',
+  isMarried: true,
+}
+
+console.log(person)
+console.log(person.firstName)
+console.log(person['lastName'])
+console.log(person.family[2])
+person.job = 'Developer'
+console.log(person)
+
+const lily = new Object()
+lily.name = 'Lily Jessica Teague'
+lily.birthYear = '2018'
+lily['job'] = 'Attends nursery'
+console.log(lily)
+
+// / Objects and methods
+const pam = {
+  firstName: 'Pamela',
+  middleName: 'Mary',
+  lastName: 'Teague',
+  birthYear: 1952,
+  family: ['Alice', 'Lily', 'Mathew', 'Tressa', 'Chloe', 'Sophie'],
+  job: 'Retired',
+  isMarried: false,
+  calcAge() {
+    this.age = new Date().getFullYear() - this.birthYear
+  },
+}
+console.log(pam.calcAge())
+console.log(pam)
+
+// / Coding Challenge 4
+const mat = {
+  firstName: 'Mathew',
+  middleName: 'Adam',
+  lastName: 'Teague',
+  birthYear: 1989,
+  family: ['Alice', 'Lily', 'Pam', 'Tressa', 'Chloe', 'Sophie'],
+  job: 'UI Developer',
+  isMarried: true,
+  calcAge() {
+    this.age = new Date().getFullYear() - this.birthYear
+  },
+  heightFt: 6.4,
+  weightSt: 15.4,
+  calcHeight() {
+    const height = this.heightFt / 3.28
+    this.height = height.toFixed(2)
+  },
+  calcWeight() {
+    const mass = this.weightSt * 6.35029318
+    this.weight = mass.toFixed(2)
+  },
+  calcBMI() {
+    this.bmi = Math.round(this.weight / (this.height * this.height))
+  },
+}
+
+const alice = {
+  firstName: 'Alice',
+  lastName: 'Teague',
+  birthYear: 1990,
+  family: ['Alice', 'Lily', 'Mathew', 'Andrew', 'Julie', 'Poppy', 'Clare'],
+  job: 'Student Support',
+  isMarried: true,
+  calcAge() {
+    this.age = new Date().getFullYear() - this.birthYear
+  },
+  heightFt: 5.4,
+  weightSt: 8,
+  calcHeight() {
+    const height = this.heightFt / 3.28
+    this.height = height.toFixed(2)
+  },
+  calcWeight() {
+    const mass = this.weightSt * 6.35029318
+    this.weight = mass.toFixed(2)
+  },
+  calcBMI() {
+    this.bmi = Math.round(this.weight / (this.height * this.height))
+  },
+}
+
+mat.calcHeight()
+mat.calcWeight()
+alice.calcHeight()
+alice.calcWeight()
+mat.calcBMI()
+alice.calcBMI()
+console.log(mat, alice)
